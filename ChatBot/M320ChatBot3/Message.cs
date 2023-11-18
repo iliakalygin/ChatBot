11c77ef3ab2561aabbf1e6bdaa2608d41305e70e
Ilia Kalygin <58369822+iliakalygin@users.noreply.github.com>
2023-11-18 01:09:28 +0100

    test

--- ChatBot/M320ChatBot3/Message.cs
+++ ChatBot/M320ChatBot3/Message.cs
@@ -6,15 +6,9 @@ using System.Threading.Tasks;
 
 namespace M320ChatBot3
 {
-    // Definiert die Message-Klasse, die ein Schlüsselwort und eine dazugehörige Antwort speichert.
     public class Message
     {
-        // Das Schlüsselwort, auf das der Chatbot reagieren soll.
-        // Das Fragezeichen markiert den String als "nullable", sodass er auch null sein kann.
         public string? Keyword { get; set; }
-
-        // Die Antwort, die der Chatbot geben soll, wenn das Schlüsselwort erkannt wird.
-        // Das Fragezeichen markiert den String als "nullable", sodass er auch null sein kann.
         public string? Answer { get; set; }
     }
 }
--- ChatBot/M320ChatBot3/Storage.cs
+++ ChatBot/M320ChatBot3/Storage.cs
@@ -8,33 +8,24 @@ using System.Reflection;
 
 namespace M320ChatBot3
 {
-    // Definiert die Storage-Klasse, die für das Laden von Nachrichten aus einer Ressourcendatei verantwortlich ist.
     public class Storage
     {
-        // Eine Liste von Message-Objekten, die die geladenen Nachrichten speichern.
         public List<Message> Messages { get; private set; } = new List<Message>();
 
-        // Die Load-Methode ist verantwortlich für das Laden der Nachrichten aus der Ressourcendatei.
         public void Load()
         {
-            // Holt die ausführende Assembly (in diesem Fall die Assembly dieser Anwendung).
             Assembly assembly = Assembly.GetExecutingAssembly();
 
-            // Definiert den Namen der Ressourcendatei, die die Nachrichten enthält.
-            string resourceName = "M320ChatBot3.responses.txt";
+            string resourceName = "ChatBot.responses.txt";
 
-            // Öffnet einen Stream zur Ressourcendatei und einen StreamReader zum Lesen des Streams.
             using (Stream? stream = assembly.GetManifestResourceStream(resourceName))
             using (StreamReader reader = new StreamReader(stream ?? throw new InvalidOperationException("Stream cannot be null")))
             {
-                // Liest jede Zeile der Ressourcendatei.
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
-                    // Zerteilt jede Zeile in Schlüsselwort und Antwort, getrennt durch ein "=".
                     var parts = line.Split('=');
 
-                    // Fügt die Nachricht zur Liste hinzu, wenn beide Teile vorhanden sind.
                     if (parts.Length == 2)
                     {
                         Messages.Add(new Message { Keyword = parts[0].Trim(), Answer = parts[1].Trim() });
